45eb86d58972eca4d059be7d4bc42900ae1d45ca
Hank <4A790162@stust.edu.tw>
2024-06-14 00:01:30 +0800

    初始化Spring Boot priject

---
+++ src/main/java/com/hank/springbootmalll/SpringbootMalllApplication.java
@@ -0,0 +1,13 @@
+package com.hank.springbootmalll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMalllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMalllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hank/springbootmalll/SpringbootMalllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hank.springbootmalll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMalllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
