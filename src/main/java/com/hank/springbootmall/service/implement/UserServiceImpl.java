0e47c7977a01f473f3b49d91e3c394845c513052
Hank <4A790162@stust.edu.tw>
2024-07-18 16:04:11 +0800

    Add error message to ResponseStatusException for email already exists

--- src/main/java/com/hank/springbootmall/service/implement/UserServiceImpl.java
+++ src/main/java/com/hank/springbootmall/service/implement/UserServiceImpl.java
@@ -10,13 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -37,7 +35,7 @@ public class UserServiceImpl implements UserService {
         Optional<User> userExist = userRepository.findByEmail(userRegisterDto.getEmail());
         if(userExist.isPresent()) {
             log.warn("Email {} 已經被註冊", userRegisterDto.getEmail());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists");
         }
         User user = new User();
         user.setCreatedDate(new Date());
