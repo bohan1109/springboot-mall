5c36e8e895294ca16f73b5c283b69cb951daa818
Hank <4A790162@stust.edu.tw>
2024-07-22 00:40:45 +0800

    Configure JWT authentication in SecurityConfig
    - Added session management policy to stateless.
    - Included JWT filter in the security filter chain before UsernamePasswordAuthenticationFilter.
    - Configured "/users/register" and "/users/login" endpoints to be accessible without authentication.
    - Ensured all other requests require authentication.
    - Disabled CSRF, form login, and HTTP Basic authentication.

--- src/main/java/com/hank/springbootmall/config/SecurityConfig.java
+++ src/main/java/com/hank/springbootmall/config/SecurityConfig.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -33,8 +35,13 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                        .requestMatchers("/users/register", "/users/login").permitAll() // Set register and login paths to not require authentication
+                        .anyRequest().authenticated() // All other requests require authentication
                 )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Set session management to stateless
+                )
+                .addFilterBefore(jwtGenericFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form.disable())
                 .httpBasic(httpBasic -> httpBasic.disable());
 
